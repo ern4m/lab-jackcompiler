f6c358fe7747868209f3377ec1cb2713a9fe76a8
ern4m <ernam.rsf@gmail.com>
2024-11-27 19:52:26 -0300

    Adição de palavras reservadas TokenType enum

--- src/main/java/br/ufma/ecp/token/TokenType.java
+++ src/main/java/br/ufma/ecp/token/TokenType.java
@@ -4,43 +4,34 @@ import java.util.List;
 import java.util.Map;
 
 public enum TokenType {
-    PLUS,MINUS,
+    PLUS, MINUS, EQ, SEMICOLON,
 
-     // Literals.
-     NUMBER,
-     STRING,
+    // Literals.
+    NUMBER, STRING, IDENT,
 
+    // keywords
+    WHILE, CLASS, CONSTRUCTOR, FUNCTION,
+    METHOD, FIELD, STATIC, VAR, INT,
+    CHAR, BOOLEAN, VOID, TRUE, FALSE,
+    NULL, THIS, LET, DO, IF, ELSE, RETURN,
 
-     IDENT,
+    EOF,
 
- 
-     // keywords
-     METHOD,
-     WHILE,
-     IF,
-     CLASS,
-     CONSTRUCTOR,
+    ILLEGAL;
 
-     EOF,
-
-     ILLEGAL;
-
-     static public boolean isSymbol (char c) {
+    static public boolean isSymbol(char c) {
         String symbols = "{}()[].,;+-*/&|<>=~";
         return symbols.indexOf(c) > -1;
     }
 
-
-    static public boolean isKeyword (TokenType type) {
-        List<TokenType> keywords  = 
-            List.of(
+    static public boolean isKeyword(TokenType type) {
+        List<TokenType> keywords = List.of(
                 METHOD,
                 WHILE,
                 IF,
                 CLASS,
-                CONSTRUCTOR
-            );
-            return keywords.contains(type);
+                CONSTRUCTOR);
+        return keywords.contains(type);
     }
 
 }
