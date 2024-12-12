ad9a8c5a947a70d64a2ec6033c66aa80dcffc847
ern4m <ernam.rsf@gmail.com>
2024-12-12 16:03:10 -0300

    fix: Configurando a classe Parser para as novas implementações

--- src/main/java/br/ufma/ecp/Parser.java
+++ src/main/java/br/ufma/ecp/Parser.java
@@ -1,64 +1,30 @@
 package br.ufma.ecp;
 
+import static br.ufma.ecp.token.TokenType.*;
+
 import br.ufma.ecp.token.Token;
 import br.ufma.ecp.token.TokenType;
 
 public class Parser {
+
+    private static class ParseError extends RuntimeException {
+    }
+
     private Scanner scan;
     private Token currentToken;
-    
+    private Token peekToken;
+
+    private StringBuilder xmlOutput = new StringBuilder();
+
     public Parser (byte[] input) {
         scan = new Scanner(input);
-        currentToken = scan.nextToken();
-        
+
+        nextToken();
     }
 
-    public void parse () {
-        expr();
-    }
-
-    void expr() {
-        number();
-        oper();
-    }
-
-    void number () {
-        System.out.println(currentToken.lexeme);
-        match(TokenType.NUMBER);
-    }
-
-    private void nextToken () {
-        currentToken = scan.nextToken();
-    }
-
-   private void match(TokenType t) {
-        if (currentToken.type == t) {
-            nextToken();
-        }else {
-            throw new Error("syntax error");
-        }
-   }
-
-    void oper () {
-        if (currentToken.type == TokenType.PLUS) {
-            match(TokenType.PLUS);
-            number();
-            System.out.println("add");
-            oper();
-        } else if (currentToken.type == TokenType.MINUS) {
-            match(TokenType.MINUS);
-            number();
-            System.out.println("sub");
-            oper();
-        } else if (currentToken.type == TokenType.EOF) {
-            // vazio
-        } else {
-            throw new Error("syntax error");
-        }
-    }
-
-    public String VMOutput() {
-        return "";
+    private void nextToken() {
+        currentToken = peekToken;
+        peekToken = scan.nextToken();
     }
 
 }
