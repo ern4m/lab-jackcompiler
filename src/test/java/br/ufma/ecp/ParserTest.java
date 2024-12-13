90cb691eb2ff16a7859fb2eb5e7518f2670f56b5
ern4m <ernam.rsf@gmail.com>
2024-12-12 22:41:09 -0300

    feature: Adicionado teste para Parser + teste do parseLet()

--- src/test/java/br/ufma/ecp/ParserTest.java
+++ src/test/java/br/ufma/ecp/ParserTest.java
@@ -7,6 +7,8 @@ import java.nio.charset.StandardCharsets;
 
 import org.junit.Test;
 
+import br.ufma.ecp.Parser;
+
 import br.ufma.ecp.token.Token;
 import br.ufma.ecp.token.TokenType;
 
@@ -16,9 +18,31 @@ public class ParserTest extends TestSupport {
 
     @Test
     public void testParseLetSimple() {
-        var input = "let string = 20;";
+        var input = "let var1 = 10+20;";
         var parser = new Parser(input.getBytes(StandardCharsets.UTF_8));
         parser.parseLet();
-        System.out.println(parser.XMLOutput());
+				var expectedResult =  """
+	     <letStatement>
+        <keyword> let </keyword>
+        <identifier> var1 </identifier>
+        <symbol> = </symbol>
+        <expression>
+          <term>
+          <integerConstant> 10 </integerConstant>
+          </term>
+          <symbol> + </symbol>
+          <term>
+          <integerConstant> 20 </integerConstant>
+          </term>
+          </expression>
+        <symbol> ; </symbol>
+      </letStatement>
+				""";
+        var result = parser.XMLOutput();
+        expectedResult = expectedResult.replaceAll("  ", "");
+        result = result.replaceAll("\r", ""); // no codigo em linux não tem o retorno de carro
+        assertEquals(expectedResult, result);
+    }
+
 
 }
